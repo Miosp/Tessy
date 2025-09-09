9775e90018fd34bcba91cd0f70b85c5b5ac25c0e
miosp <jan.ludwina@gmail.com>
2025-09-09 18:04:37 +0200

    add colored output support for task logging and update .gitignore

--- src/tasks/task.rs
+++ src/tasks/task.rs
@@ -1,8 +1,28 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+use colored::{Color, Colorize};
 use hashlink::LinkedHashMap;
 use saphyr::{Scalar, Yaml};
 use snafu::Snafu;
 
 use crate::tasks::{ExecuteTask, ExecuteTaskError};
+
+pub fn print_from_task(
+    id: impl AsRef<str>,
+    color: Color,
+    message: impl AsRef<str>,
+) {
+    let task_info = format!("[{}]", id.as_ref());
+
+    match supports_color::on_cached(supports_color::Stream::Stdout) {
+        Some(support) if support.has_16m => {
+            println!("{}: {}", task_info.color(color), message.as_ref());
+        }
+        _ => {
+            println!("{}: {}", task_info, message.as_ref());
+        }
+    }
+}
 
 pub trait TaskTrait {
     fn from_task_yaml(task_name: &str, task_data: &LinkedHashMap<Yaml, Yaml>) -> Option<Self>
@@ -13,6 +33,19 @@ pub trait TaskTrait {
     fn id(&self) -> String;
     fn dependencies(&self) -> &Vec<String>;
     fn inputs(&self) -> &Vec<String>;
+    fn color(&self) -> Color {
+        let mut hasher = DefaultHasher::new();
+        self.id().hash(&mut hasher);
+        let hash = hasher.finish();
+        let red = (hash & 0xFF) as u8;
+        let green = ((hash >> 8) & 0xFF) as u8;
+        let blue = ((hash >> 16) & 0xFF) as u8;
+        Color::TrueColor {
+            r: red,
+            g: green,
+            b: blue,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
